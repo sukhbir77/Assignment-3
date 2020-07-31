29290a47c86e4fc4e8d31b460dfae08376f8b41f
Sukhbir Singh <44106929+sukhbir77@users.noreply.github.com>
2020-07-31 15:25:19 -0700

    Add files via upload

---
+++ assets/data/myBooks.ts
@@ -0,0 +1,34 @@
+import {bookInfo} from '../../app/bookInfo';
+
+export const MYCLASSES: bookInfo [] = 
+[
+  {
+    book_name:'Harry Potter and the Order of the Phoenix',
+    author: 'Mary GrandPré',
+    genre: 'Fantasy',
+    publish_year: '2004',
+    picture: 'assets/images/image.jfif'
+  },
+  {
+    book_name:'To Kill a Mockingbird',
+    author: 'Harper Lee',
+    genre: 'Horror',
+    publish_year: '2006',
+    picture: 'assets/images/image2.jpg'
+  },
+  {
+    book_name:'Twilight',
+    author: 'Stephenie Meyer',
+    genre: 'Drama-Romance',
+    publish_year: '2006',
+    picture: 'assets/images/image3.jpg'
+  },
+  {
+    book_name:'Jane Eye',
+    author: 'Charlotte Bronte',
+    genre: 'Romance',
+    publish_year: '2002',
+    picture: 'assets/images/image4.jpg'
+  }
+  
+];
